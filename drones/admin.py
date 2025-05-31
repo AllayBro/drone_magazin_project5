c3c1175fd9ba1be43b12871fd293c4847f8904a3
Артем Зотов <178171242+AllayBro@users.noreply.github.com>
2025-05-31 22:28:09 +0300

    Delete drones directory

--- drones/admin.py
+++
@@ -1,7 +0,0 @@
-
-from django.contrib import admin
-from .models import Drone, Booking  # Импортируем только модели дронов
-
-admin.site.register(Drone)
-admin.site.register(Booking)
-
--- drones/utils.py
+++
@@ -1,6 +0,0 @@
-from django.db.models import Avg
-from .models import Drone, DroneRating
-
-def update_drone_rating(drone_id):
-    avg = DroneRating.objects.filter(drone_id=drone_id).aggregate(Avg('score'))['score__avg']
-    Drone.objects.filter(id=drone_id).update(rating=avg)
